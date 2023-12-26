f32f946a91a6663852c280507de865258228976d
rutudadhania <154891333+rutudadhania@users.noreply.github.com>
2023-12-26 17:44:14 +0530

    Details of myself

---
+++ detail.php
@@ -0,0 +1,5 @@
+<?php
+echo"<b><font color='Blue'>My name is Rutu Dadhania<br></font></b>";
+echo"<b><font color='Orange'>My email id is rutudadhania9576@gmail.com<br></b></font>";
+echo"<i><font color='Red'><b>I live in Rajkot</b></font></i>";
+?>
