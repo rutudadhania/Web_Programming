847ced6e43860e9123490808688d7640cd43b320
rutudadhania <154891333+rutudadhania@users.noreply.github.com>
2024-01-14 09:36:32 +0530

    Add files via upload

---
+++ Dynamic_Table/table1.php
@@ -0,0 +1,16 @@
+<?php 
+$start= $_GET['startval'];
+$end= $_GET['endval'];
+echo "<table border=1 bgcolor='pink'>";
+
+for($i=1;$i<=10;$i++)
+{   echo"<tr>";
+	for($j=$start;$j<=$end;$j++)
+    {   
+    
+    $m=$j*$i;
+    echo "<td>$j</td> <td>X</td> <td>$i</td> <td>=</td> <td>$m</td><td bgcolor='yellow'></td>";
+	}
+	echo "<tr>";
+}
+?>
